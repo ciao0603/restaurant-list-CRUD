f9c86f23a720f5c8b692681a7ba8b3494f327ec7
ciao0603 <jocelyn94032.0@gmail.com>
2023-07-05 13:23:50 +0800

    feat: add edit restaurant function

--- app.js
+++ app.js
@@ -22,22 +22,59 @@ db.once('open', () => {
 // 設定view引擎
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+// 套入靜態css
 app.use(express.static('public'))
+// 載入body-parser
+app.use(express.urlencoded({extended: true}))
 
 // 設定路由
+// 瀏覽
 app.get('/', (req, res) => {
   Data.find()
     .lean()
     .then(datas => res.render('index', {datas}))
     .catch(error => console.log(error))
 })
-
+// 簡介
 app.get('/restaurants/:id', (req, res) => {
   const id = req.params.id
   Data.findById(id)
     .lean()
     .then(data => res.render('show', { data }))
     .catch(error => console.log(error))
+})
+// 編輯-get
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  Data.findById(id)
+    .lean()
+    .then(data => res.render('edit', { data }))
+    .catch(error => console.log(error))
+})
+// 編輯-post
+app.post('/restaurants/:id/edit', (req, res) => {
+const id = req.params.id
+const name = req.body.name
+const category = req.body.category
+const location = req.body.location
+const google_map = req.body.google_map
+const phone = req.body.phone
+const description = req.body.description
+const image = req.body.image
+
+Data.findById(id)
+  .then(data => {
+    data.name = name
+    data.category = category
+    data.location = location
+    data.google_map = google_map
+    data.phone = phone
+    data.description = description
+    data.image = image
+    return data.save()
+  })
+  .then(() => res.redirect(`/restaurants/${id}`))
+  .catch(error => console.log(error))
 })
 
 // 監聽伺服器
