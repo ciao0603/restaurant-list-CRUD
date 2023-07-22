5699d4e00338775d80986d6117b111846d255f6b
ciao0603 <jocelyn94032.0@gmail.com>
2023-07-22 21:46:34 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -38,4 +38,10 @@ router.post('/register', (req, res) => {
     .catch(err => console.log(err))
 })
 
+// logout
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/users/login')
+})
+
 module.exports = router
