a08697113023391c0b9c6d26fa1a39c03b309610
Jocelyn Liu <jocelyn94032.0@gmail.com>
2023-07-22 19:50:31 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
+
 const routes = require('./routes')
 const app = express()
 const port = 3000
@@ -18,6 +20,13 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 // 載入method-override
 app.use(methodOverride('_method'))
+// 設定session
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
 
 // 設定路由
 app.use(routes)
