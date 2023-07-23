a83c2db31d8fc05302caff70926d5a8b0fd608bc
Jocelyn Liu <jocelyn94032.0@gmail.com>
2023-07-23 16:17:38 +0800

    feat: add bcrypt to login route

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcryptjs')
 
 const User = require('../models/user')
 
@@ -8,17 +9,21 @@ module.exports = (app) => {
   app.use(passport.initialize())
   app.use(passport.session())
   // 設定本地登入策略
-  passport.use(new LocalStrategy({usernameField:'email'}, (email, password, done) => {
+  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
     User
-      .findOne({email})
-      .then(user=>{
-        if(!user) {
-          return done(null, false, {message: 'can not find user'})
+      .findOne({ email })
+      .then(user => {
+        if (!user) {
+          return done(null, false, { message: 'can not find user' })
         }
-        if(user.password !== password) {
-          return done(null, false, {message: 'email or password incorrect'})
-        }
-        return done(null, user)
+        return bcrypt
+          .compare(password, user.password) //hash要放後面
+          .then(isMatch => {
+            if (!isMatch) {
+              return done(null, false, { message: 'email or password incorrect' })
+            }
+            return done(null, user)
+          })
       })
       .catch(err => done(err, false))
   }))
@@ -26,7 +31,7 @@ module.exports = (app) => {
   passport.serializeUser((user, done) => {
     done(null, user._id)
   })
-  passport.deserializeUser((id, done)=>{
+  passport.deserializeUser((id, done) => {
     User
       .findById(id)
       .lean()
